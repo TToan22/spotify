cd9f26be79c652e0dcad1e7f02e93a64c1b7a727
vuongducbao <94464272+vuongducbao@users.noreply.github.com>
2021-12-27 10:10:17 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,9 +1,15 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link rel="alternate" href="css/filee.css" type="application/atom+xml" title="Atom">
-<!------ Include the above in your HEAD tag ---------->
-
+</head>
+<body>
 <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-4 well well-sm">
@@ -56,3 +62,6 @@
         </div>
     </div>
 </div>
+</body>
+</html>
+
