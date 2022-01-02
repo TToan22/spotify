8f34d37f8adc91fa6f343747a05456063c8a0b4d
vuongducbao <94464272+vuongducbao@users.noreply.github.com>
2022-01-03 00:23:04 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,23 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng nhập</title>
+    <title>Sign up</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
   <header>
-    <img src = "https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"/></header>
-
-    <hr>
-    
+    <img src = "https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"/>
+  </header>    
     <div class = "container">
       <h2 class = "tit1" >Sign up for free to start listening.</h2>
-    
-      <br>
-    <hr/>
-    <br>
+    </div>
+  <main>
     <form>
       <div class="form-group">
         <label for="exampleInputEmail1">What's your email?</label>
@@ -38,9 +34,12 @@
         <label for="exampleInputPassword1">What should we call you?</label>
         <input type="password" class="form-control" id="exampleInputPassword1"  placeholder="Enter a profile name.">
       </div>   
+    </form>
       <div>
           <label for="exampleInputEmail1">What's your date of birth?</label>
       </div> 
+   
+
       <div>
         <div class="row">
           <div class="col-xs-2 col-md-2">
@@ -95,8 +94,8 @@
         <label class="form-check-label" for="exampleCheck2">Share my registration data with Spotify's content providers for marketing purposes.</label>
       </div>
       <button type="submit" class="btn btn-primary" name="btnSignIn" id="submit">Sign up</button>
-    </form>
     </div>
+  </main>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
