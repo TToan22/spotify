6a24d4d7d181150b12aef2881b54659853c0a094
vuongducbao <94464272+vuongducbao@users.noreply.github.com>
2022-01-03 23:42:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -17,7 +17,7 @@
     
     <div class = "container">
       <h2 class = "tit1">Để tiếp tục, hãy đăng nhập vào Spotify</h2>
-      <button type="FB" class = "btnA" id = "facebook" >TIẾP TỤC VỚI FACEBOOK</button>
+      <button  class = "btnA" id = "facebook" >TIẾP TỤC VỚI FACEBOOK</button>
       <button class = "btnA" id = "apple">TIẾP TỤC VỚI APPLE</button>
       <button class = "btnA" id = "google">TIẾP TỤC BẰNG GOOGLE</button>
       <button class = "btnA" id = "telephone">TIẾP TỤC VỚI SỐ ĐIỆN THOẠI</button>
