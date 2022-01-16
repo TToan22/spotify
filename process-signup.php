3e0d5982e129f790cb82e6b2cd26c93e45086779
vuongducbao <94464272+vuongducbao@users.noreply.github.com>
2022-01-17 00:25:03 +0700

    Update process-signup.php

--- process-signup.php
+++ process-signup.php
@@ -6,7 +6,7 @@
         $user = $_POST['txtUser'];
         $email = $_POST['txtEmail'];
         $email1 = $_POST['txtEmail1'];
-        $pass1 = $_POST['txtPass'];
+        $pass = $_POST['txtPass'];
         // Bước 01: Kết nối Database Server
         $conn = mysqli_connect('localhost','root','','spotify');
         if(!$conn){
@@ -22,12 +22,12 @@
             $error = "Tài khoản hoặc mật khẩu không tồn tại";
             header("location: Signup.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
         }else{
-            $pass_md5 = md5($pass1);
-            $sql02 = "INSERT INTO db_nguoidung(tendangnhap , email, matkhau) VALUE('$user' ,'$email1','$pass_md5')";
+            $pass_hash = $pass;
+            $sql02 = "INSERT INTO db_nguoidung(tendangnhap , email, matkhau) VALUE('$user' ,'$email1','$pass_hash')";
             $result02 = mysqli_query($conn,$sql02);
         }
         if($result02== true) {
-            header("location:Signup.php");
+            header("location:home.php");
 
         }
         else{
