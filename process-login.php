48736d65910ebbc69a866441fa7734a944a93550
vuongducbao <94464272+vuongducbao@users.noreply.github.com>
2022-01-17 00:25:01 +0700

    Update process-login.php

--- process-login.php
+++ process-login.php
@@ -7,30 +7,41 @@
         $email = $_POST['txtEmail'];
         $pass  = $_POST['txtPass'];
         //Ở đây còn phải kiểm tra người dùng đã nhập chưa
-
+    
         // Bước 01: Kết nối Database Server
-        $conn = mysqli_connect('localhost','root','','spotify');
+        $conn = mysqli_connect('localhost','root','','spotify'); 
         if(!$conn){
             die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
         }
+        
+        
         // Bước 02: Thực hiện truy vấn
-        $sql = "SELECT * FROM db_nguoidung WHERE email = '$email' AND matkhau='$pass'";
+        $sql = "SELECT * FROM db_nguoidung WHERE email = '$email'";
         // Ở đây còn có các vấn đề về tính hợp lệ dữ liệu nhập vào FORM
         // Nghiêm trọng: lỗi SQL Injection
 
         $result = mysqli_query($conn,$sql);
+   
         if(mysqli_num_rows($result) > 0){
+            $rowresult = mysqli_fetch_assoc($result);
+            if  ($pass=$rowresult['matkhau']) {
+                $_SESSION['isLoginOK'] = $email;
+                header("location: index.php");
+                
+            }
+            else {
+                $error = "Bạn nhập thông tin Email hoặc mật khẩu chưa chính xác";
+                header("location: login.php?error=$error");
+            } 
             // CẤP THẺ LÀM VIỆC
-            $_SESSION['isLoginOK'] = $email;
-            header("location: index.php"); //Chuyển hướng về Trang quản trị
+             //Chuyển hướng về Trang quản trị
         }else{
-            $error = "Bạn nhập thông tin Email hoặc mật khẩu chưa chính xác";
-            header("location: login.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
+             echo "query bị lỗi" ;    //Chuyển hướng, hiển thị thông báo lỗi
         }
 
         // Bước 03: Đóng kết nối
         mysqli_close($conn);
     }else{
         header("location:login.php");
-    }
+     }
 ?>
